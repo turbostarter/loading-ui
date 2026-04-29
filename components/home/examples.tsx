46c5aa8a0f1f67dad3f0d5818e634b0b8caf28f9
Moamal-2000 <moamalalaapro1@gmail.com>
2026-04-29 07:50:20 +0300

    fix(examples): show copy icons when parent element receives focus

--- components/home/examples.tsx
+++ components/home/examples.tsx
@@ -39,7 +39,7 @@ export const Examples = async () => {
             key={name}
             className="group/component relative aspect-4/3 w-full max-w-full shrink-0 grow-0 sm:w-[calc((100%-1px)/2)] md:w-[calc((100%-2px)/3)] lg:w-[calc((100%-3px)/4)]"
           >
-            <div className="absolute inset-x-0 top-0 z-10 flex items-center justify-end p-4 opacity-0 transition-opacity group-hover/component:opacity-100 pointer-coarse:opacity-100">
+            <div className="absolute inset-x-0 top-0 z-10 flex items-center justify-end p-4 opacity-0 transition-opacity group-focus-within/component:opacity-100 group-hover/component:opacity-100 pointer-coarse:opacity-100">
               <div className="flex items-center gap-1.5">
                 <CopyComponent item={item} />
                 <CopyCLICommand
