f68941352441e7d6b0bcbe9cec4159ebd7e4338a
Akshar Barot <akshar.barot67@gmail.com>
2026-05-03 05:35:02 -0400

    fix TurboStarter link in footer (#11)

--- components/common/header/search.tsx
+++ components/common/header/search.tsx
@@ -14,13 +14,16 @@ export const Search = ({
 
   return (
     <Button
-      className={cn("text-muted-foreground relative max-w-52 min-w-15 w-full shrink", className)}
+      className={cn(
+        "text-muted-foreground relative w-full max-w-52 min-w-15 shrink",
+        className,
+      )}
       onClick={() => setOpenSearch(true)}
       variant="secondary"
       {...props}
     >
       <SearchIcon className="size-4" />
-      <p className="font-normal hidden lg:block">Search...</p>
+      <p className="hidden font-normal lg:block">Search...</p>
       <span className="ml-auto flex items-center gap-0.5">
         <Kbd className="bg-background border">⌘</Kbd>
         <Kbd className="bg-background border">K</Kbd>
